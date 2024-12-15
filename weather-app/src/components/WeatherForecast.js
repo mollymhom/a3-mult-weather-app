ecaa4c6b4595d75ca9c9e39cf316b5d3c3372052
mollymhom <wetanwarat@gmail.com>
2024-12-15 15:57:38 -0600

    Add more components: WeatherForecast.css and .js

---
+++ weather-app/src/components/WeatherForecast.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './WeatherForecast.css';
+
+function WeatherForecast({ forecast }) {
+  return (
+    <div className="weather-forecast">
+      <h3>3-Day Forecast</h3>
+      <ul>
+        {forecast.map((day, index) => (
+          <li key={index}>
+            <strong>{day.date}</strong>: {day.condition}, {day.temp}°C
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default WeatherForecast;
